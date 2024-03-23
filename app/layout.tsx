b2ad2fcd0798577e35fdaae8fdcecadc21b7ab42
angelDova <Angelfcb01@yahoo.com>
2024-03-22 17:39:34 -0700

    Exit Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import { ExitModal } from "@/components/modals/exit-modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={font.className}>
           <Toaster richColors position="top-center" />
+          <ExitModal />
           {children}
         </body>
       </html>
---
+++ app/lesson/header.tsx
@@ -0,0 +1,42 @@
+import { Progress } from "@/components/ui/progress";
+import { useExitModal } from "@/store/use-exit-modal";
+import { InfinityIcon, X } from "lucide-react";
+import Image from "next/image";
+
+type Props = {
+  hearts: number;
+  percentage: number;
+  hasActiveSubscription: boolean;
+};
+
+export const Header = ({
+  hearts,
+  percentage,
+  hasActiveSubscription,
+}: Props) => {
+  const { open } = useExitModal();
+
+  return (
+    <header className="lg:pt[50px] mx-auto flex w-full max-w-[1140px] items-center justify-between gap-x-7 px-10 pt-[20px]">
+      <X
+        className="cursor-pointer text-slate-500 transition hover:opacity-75"
+        onClick={open}
+      />
+      <Progress value={percentage} />
+      <div className="flex items-center font-bold text-rose-500">
+        <Image
+          src={"/heart.svg"}
+          height={28}
+          width={28}
+          alt="Heart"
+          className="mr-2"
+        />
+        {hasActiveSubscription ? (
+          <InfinityIcon className="h-6 w-6 stroke-[3]" />
+        ) : (
+          hearts
+        )}
+      </div>
+    </header>
+  );
+};
